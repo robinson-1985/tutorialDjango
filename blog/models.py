9f24fa3bc0b8eecf598c6016aef9a453a967cafd
robinson <https://github.com/robinson-1985>
2022-01-19 13:01:20 -0300

    Subindo arquivos Heroku

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse 
 
-# Create your models here:
+
 class Post(models.Model):
    title= models.CharField(max_length=255)
    slug = models.SlugField(max_length=255, unique=True)
