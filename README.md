f1f6c48396cf782047a54a5f7ff7ad45e6beedac
robinson <https://github.com/robinson-1985>
2022-01-19 12:57:32 -0300

    Subindo arquivos Heroku

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tutorialDjango
+
+Author: Robinson Dias.
+
+Python Django Tutorial em Português – Introdução ao framework – Criando um blog com Python e Django
+
+Código [desta aula](https://youtu.be/Dzuiy-JNi-E).
---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    #abaixo edito os campos que quero exibir na tela:
+    list_display = ("title", "slug", "author", "created", "updated")
+    prepopulated_fields = {"slug": ("title" ,)}
